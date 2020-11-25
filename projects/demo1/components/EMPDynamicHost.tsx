48fb7da355b801fb91bf6552197223eed04faaa7
xuhongbin <xuhongbin@yy.com>
2020-11-25 20:27:36 +0800

    feat: 完成动态host 调用

---
+++ projects/demo1/components/EMPDynamicHost.tsx
@@ -0,0 +1,110 @@
+import React from 'react'
+interface HostConfig {
+  name: string
+  module: string
+}
+interface RemoteHostProps {
+  host: HostConfig
+  widgetProps?: any
+}
+interface tipsI {
+  noHost: string | React.ReactNode
+  failed: string | React.ReactNode
+  loading: string | React.ReactNode
+}
+interface RemoteHostsPrividerProps {
+  tips?: tipsI
+  hosts: Array<string>
+  children?: any
+}
+
+const loadComponent = (remote: string, module: string) => {
+  return async () => {
+    const factory = await (window as any)[remote].get(module)
+    return factory()
+  }
+}
+const remoteHosts: any = {}
+const registerHost = (url: string) => {
+  remoteHosts[url] = {}
+  remoteHosts[url].element = document.createElement('script')
+  remoteHosts[url].element.src = url
+  remoteHosts[url].element.type = 'text/javascript'
+  remoteHosts[url].element.async = true
+  document.head.appendChild(remoteHosts[url].element)
+  return new Promise((resolve, reject) => {
+    remoteHosts[url].element.onload = () => {
+      resolve(true)
+    }
+    remoteHosts[url].element.onerror = () => {
+      reject(false)
+    }
+  })
+}
+const unregisterAllHost = () => {
+  for (const url in remoteHosts) {
+    if (remoteHosts[url].element && remoteHosts[url].element.src) document.head.removeChild(remoteHosts[url].element)
+  }
+}
+
+const useDynamicScript = (hosts: Array<string>) => {
+  const [ready, setReady] = React.useState(false)
+  const [failed, setFailed] = React.useState(false)
+
+  React.useEffect(() => {
+    const registerAllhost = hosts.map(url => registerHost(url))
+    Promise.all(registerAllhost)
+      .then(rs => {
+        const isFail = rs.includes(false)
+        if (isFail) {
+          setFailed(true)
+        } else {
+          setReady(true)
+        }
+      })
+      .catch(e => console.error(e))
+
+    return () => {
+      unregisterAllHost()
+    }
+  }, [hosts])
+
+  return {
+    ready,
+    failed,
+  }
+}
+const NoHostComp = () => <>No Host.</>
+const LoadComp = () => <>Loading...</>
+const LoadFailComp = () => <>Loading failed</>
+export const EMPHostsProvider: React.FC<RemoteHostsPrividerProps> = ({
+  hosts,
+  children,
+  tips = {
+    noHost: <NoHostComp />,
+    failed: <LoadFailComp />,
+    loading: <LoadComp />,
+  },
+}: RemoteHostsPrividerProps) => {
+  const {ready, failed} = useDynamicScript(hosts)
+  if (hosts.length === 0) {
+    return <div>{tips.noHost}</div>
+  }
+
+  if (failed) {
+    return <div>{tips.failed}</div>
+  }
+
+  if (!ready) {
+    return <div>{tips.loading}</div>
+  }
+  return children
+}
+export const EMPComponent: React.FC<RemoteHostProps> = ({host, widgetProps}: RemoteHostProps) => {
+  const Component = React.lazy(loadComponent(host.name, host.module))
+  return (
+    <React.Suspense fallback="Loading Button">
+      <Component {...widgetProps} />
+    </React.Suspense>
+  )
+}
--- projects/demo1/components/home.tsx
+++ projects/demo1/components/home.tsx
@@ -1,10 +1,16 @@
-import EMPDynamic from './EMPDynamic'
+import {EMPComponent, EMPHostsProvider} from './EMPDynamicHost'
 const Home = () => (
-  <>
+  <EMPHostsProvider hosts={['http://localhost:8000/emp.js']}>
     <h1>Demo 1 Components</h1>
     <div>
-      <EMPDynamic host={{url: 'http://localhost:8000/emp.js', name: 'demo2', module: './home'}} />
+      <EMPComponent host={{name: 'demo2', module: './home'}} />
+      <EMPComponent host={{name: 'demo2', module: './home'}} />
+      <EMPComponent host={{name: 'demo2', module: './home'}} />
+      <EMPComponent host={{name: 'demo2', module: './home'}} />
+      <EMPComponent host={{name: 'demo2', module: './home'}} />
+      <EMPComponent host={{name: 'demo2', module: './home'}} />
+      <EMPComponent host={{name: 'demo2', module: './home'}} />
     </div>
-  </>
+  </EMPHostsProvider>
 )
 export default Home
