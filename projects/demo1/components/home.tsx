6e4222f98e834a08d9f63de4adc43385941edf96
xuhongbin <xuhongbin@yy.com>
2020-12-01 12:23:51 +0800

    up

--- projects/demo1/components/home.tsx
+++ projects/demo1/components/home.tsx
@@ -1,9 +1,12 @@
 // import D2Home from '@d2/home'
 import D2Dynamic from './DynamicComp'
+// import {EMPImport} from './EMPDynamicHost'
 const Home = () => (
   <>
+    <h1>Nextjs app</h1>
     {/* <D2Home title={'demo2'} /> */}
     <D2Dynamic />
+    {/* <EMPImport mod={() => import('@d2/home')} props={{title: 'use EmpImport Component!'}} /> */}
   </>
 )
 export default Home
