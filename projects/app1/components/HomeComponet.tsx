714b80e65953e98779d3e7abb060b7c5093eed54
xuhongbin <xuhongbin@yy.com>
2021-04-26 15:01:04 +0800

    refactor: 修正放阿飞

--- projects/app1/components/HomeComponet.tsx
+++ projects/app1/components/HomeComponet.tsx
@@ -1,12 +1,25 @@
 import dynamic from 'next/dynamic'
 
 const DynamicMF = dynamic(() =>
-  import('components/dynamic/loadMod').then().then(d => {
-    console.log(d)
-    return d.default
+  import('components/empComponent/helper/loadComponent')
+    .then(({importMFComponent}) =>
+      importMFComponent({
+        url: 'http://localhost:3003/emp.js',
+        scope: 'staticHost',
+        module: './home',
+      }),
+    )
+    .then(d => {
+      // console.log('mf', d.default)
+      return d
+    }),
+)
+const Head = dynamic(() =>
+  import('components/dynamic/Head').then((mod: any) => {
+    // console.log('local', mod.default)
+    return mod.default
   }),
 )
-const Head = dynamic(() => import('components/dynamic/Head').then((mod: any) => mod.default))
 const HomeComponet = () => {
   return (
     <>
--- projects/app1/components/empComponent/helper/loadComponent.ts
+++ projects/app1/components/empComponent/helper/loadComponent.ts
@@ -1,4 +1,9 @@
 import serverLoadComponent from 'components/empComponent/helper/serverLoadComponent'
 import clienLoadComponent from 'components/empComponent/helper/clienLoadComponent'
+import {LoadComponentI} from 'components/empComponent/types/index'
 const loadComponent = typeof window === 'undefined' ? serverLoadComponent : clienLoadComponent
 export default loadComponent
+export const importMFComponent = async function (system: LoadComponentI) {
+  const mod = await loadComponent(system)()
+  return mod
+}
